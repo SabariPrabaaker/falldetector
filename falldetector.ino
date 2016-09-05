0b6c4666ec77d16d08e818c52460a75bb1ef9ec8
SabariPrabaaker <sabari.prabaaker@gmail.com>
2016-09-05 09:41:39 +0530

    arduino code

---
+++ falldetector.ino
@@ -0,0 +1,90 @@
+//////////////////////////////////////////////////////////////////
+//©2011 bildr
+//Released under the MIT License - Please reuse change and share
+//Simple code for the ADXL335, prints calculated orientation via serial
+//////////////////////////////////////////////////////////////////
+
+
+//Analog read pins
+const int xPin = 0;
+const int yPin = 1;
+const int zPin = 2;
+
+//The minimum and maximum values that came from
+//the accelerometer while standing still
+//You very well may need to change these
+int minVal = 265;
+int maxVal = 320;
+
+
+//to hold the caculated values
+double x;
+double y;
+double z;
+double x1;
+double y1;
+double z1;
+
+
+void setup(){
+  Serial.begin(9600); 
+  int xRead = analogRead(xPin);
+  int yRead = analogRead(yPin);
+  int zRead = analogRead(zPin);
+   int xAng = map(xRead, minVal, maxVal, -90, 90);
+  int yAng = map(yRead, minVal, maxVal, -90, 90);
+  int zAng = map(zRead, minVal, maxVal, -90, 90);
+   x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+  pinMode(13,OUTPUT);
+  x1=x;
+  y1=y;
+  z1=z;
+}
+
+
+void loop(){
+  
+
+  //read the analog values from the accelerometer
+  int xRead = analogRead(xPin);
+  int yRead = analogRead(yPin);
+  int zRead = analogRead(zPin);
+
+  //convert read values to degrees -90 to 90 - Needed for atan2
+  int xAng = map(xRead, minVal, maxVal, -90, 90);
+  int yAng = map(yRead, minVal, maxVal, -90, 90);
+  int zAng = map(zRead, minVal, maxVal, -90, 90);
+
+  //Caculate 360deg values like so: atan2(-yAng, -zAng)
+  //atan2 outputs the value of -π to π (radians)
+  //We are then converting the radians to degrees
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+  
+  //Output the caculations
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print(" | y: ");
+  Serial.print(y);
+  Serial.print(" | z: ");
+  Serial.println(z);
+  if(x>(1.5*x1)&&y>(1.5*y1)&&z>(1.5*z1))
+  {
+    Serial.println("FALL");
+  digitalWrite(13,HIGH);
+  delay(5000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  }
+  
+ x1=x;
+ y1=y;
+ z1=z;
+  
+  
+  delay(100);//just here to slow down the serial output - Easier to read
+  
+}
